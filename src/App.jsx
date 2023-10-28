ee507372969def795c001324bfe7d26554060d2d
Gabiconiglio <111024744+Gabiconiglio@users.noreply.github.com>
2023-10-28 14:58:49 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from"./Components/Navbar/Navbar.jsx";
+import Footer from"./Components/Footer/Footer.jsx";
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
